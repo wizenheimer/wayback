a4e77b81ffbf9c37a533f91b3d5a91e63976e321
wizenheimer <xNayanKumar@gmail.com>
2024-11-23 21:18:13 +0530

    docs: sync docs for workers

--- archive/src/utils/encoding.ts
+++
@@ -1,8 +0,0 @@
-export const encodeBase64 = (arrayBuffer: ArrayBuffer): string => {
-  const bytes = new Uint8Array(arrayBuffer);
-  const binaryString = bytes.reduce(
-    (str, byte) => str + String.fromCharCode(byte),
-    ""
-  );
-  return btoa(binaryString);
-};
