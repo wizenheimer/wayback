673a636fd6ae1d01ebc12b9583cbd3c1f0d5fb03
wizenheimer <xNayanKumar@gmail.com>
2024-11-25 00:41:58 +0530

    fix: refactor diffing service

--- src/utils/initializer.ts
+++ src/utils/initializer.ts
@@ -4,18 +4,27 @@ import { ScreenshotService } from "../service/screenshot";
 import { StorageService } from "../service/storage";
 import { AIService } from "../service/ai";
 import { CompetitorService } from "../service/competitor";
+import { DiffService } from "../service/diff";
 
+// Initializes core services
 const initializeServices = (c: any) => {
+  // Initialize Screenshot Service
   const storage = new StorageService(c.env.archive);
-  const screenshot = new ScreenshotService(
+  const screenshotService = new ScreenshotService(
     c.env.SCREENSHOT_SERVICE_API_KEY,
     c.env.SCREENSHOT_SERVICE_ORIGIN,
     storage
   );
+
+  // Initialize Competitor Service
+  const competitorService = new CompetitorService(c.env.COMPETITOR_DB);
+
+  // Initialize Diff Service
   const diffDB = new DBService(c.env.DIFF_DB);
   const ai = new AIService(c.env.OPENAI_API_KEY);
-  const competitor = new CompetitorService(c.env.COMPETITOR_DB);
-  return { screenshot, diffDB, ai, competitor };
+  const diffService = new DiffService(screenshotService, diffDB, ai);
+
+  return { screenshotService, diffService, competitorService };
 };
 
 export { initializeServices };
