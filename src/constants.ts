e7ff0ecf8498ea859703b6a13a1988e54f89dc16
wizenheimer <xNayanKumar@gmail.com>
2024-11-24 21:16:09 +0530

    feat: reorg the handlers into constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,60 @@
-const version = "/api/v1";
+const docsStub = "/docs";
 
-const diffHistoryEndpoint = `${version}/diff/history`;
+const baseStub = "/api";
 
-const diffCreationEndpoint = `${version}/diff/create`;
+const versionStub = `${baseStub}/v1`;
 
-const screenshotContentQueryEndpoint = `${version}/content/:hash/:date`;
+// ==================== Screenshots ====================
 
-const screenshotImageQueryEndpoint = `${version}/screenshot/:hash/:date`;
+// Get the content of a screenshot
+const screenshotContentQueryEndpoint = `${versionStub}/content/:hash/:date`;
 
-const screenshotCreationEndpoint = `${version}/screenshot`;
+// Get the image of a screenshot
+const screenshotImageQueryEndpoint = `${versionStub}/screenshot/:hash/:date`;
 
-const reportCreationEndpoint = `${version}/report`;
+// Create a new screenshot
+const screenshotCreationEndpoint = `${versionStub}/screenshot`;
+
+// ==================== Diff ====================
+
+// Get the history of a diff
+const diffHistoryEndpoint = `${versionStub}/diff/history`;
+
+// Create a new diff
+const diffCreationEndpoint = `${versionStub}/diff/create`;
+
+// ==================== Reports ====================
+
+// Create a new report
+const reportCreationEndpoint = `${versionStub}/report`;
+
+// ==================== Competitors Aggregate ====================
+
+// Create a new competitor
+const createCompetitorEndpoint = `${versionStub}/competitors`;
+
+// List all competitors
+const listCompetitorsEndpoint = `${versionStub}/competitors`;
+
+// List competitor URLs with pagination and optional domain hash filter
+const listCompetitorsURLs = `${versionStub}/competitors/url`;
+
+// List competitor URLs by hash
+const listCompetitorsbyHash = `${versionStub}/competitors/hash/:hash`;
+
+// ==================== Competitors Individual ====================
+
+// Update any property in an existing competitor
+const updateCompetitorEndpoint = `${versionStub}/competitors/id/:id`;
+
+// Update the URL of a competitor
+const updateCompetitorURLEndpoint = `${versionStub}/competitors/id/:id/url`;
+
+// Delete entire competitor info including URLs
+const deleteCompetitorEndpoint = `${versionStub}/competitors/id/:id`;
+
+// Get competitor info
+const getCompetitorEndpoint = `${versionStub}/competitors/id/:id`;
 
 export {
   diffHistoryEndpoint,
@@ -19,4 +63,14 @@ export {
   screenshotImageQueryEndpoint,
   screenshotCreationEndpoint,
   reportCreationEndpoint,
+  docsStub,
+  baseStub,
+  createCompetitorEndpoint,
+  updateCompetitorEndpoint,
+  deleteCompetitorEndpoint,
+  getCompetitorEndpoint,
+  listCompetitorsEndpoint,
+  listCompetitorsURLs,
+  listCompetitorsbyHash,
+  updateCompetitorURLEndpoint,
 };
